5a3c0c594ed3b30a42d39f84d6742992ac2691ec
Sakai-san <o_edo@hotmail.com>
2021-10-12 22:22:17 +0200

    TYping.

--- pyrene/src/components/Filter/types.ts
+++ pyrene/src/components/Filter/types.ts
@@ -5,21 +5,21 @@ import { Option } from '../MultiSelect/types';
 export type MultiSelectOption = Option;
 export type SingleSelectOption = Array<SingleSelectOpt<DefaultValueType>>;
 
-export type SingleSelectValue = SingleSelectOpt<DefaultValueType>;
 export type MultiselectValue = ReadonlyArray<MultiSelectOption>;
+export type SingleSelectValue = SingleSelectOpt<DefaultValueType>;
 export type InputFieldValue = DefaultValueType;
 
-export type Value = MultiselectValue | SingleSelectValue | InputFieldValue;
+export type Options = MultiselectValue | SingleSelectValue | InputFieldValue;
 
 export type Filter = {
   id: string,
   label: string,
   negated?: boolean,
-  options: Value,
+  options: Options,
   sorted?: boolean,
   type: 'singleSelect' | 'multiSelect' | 'text',
 };
 
-export type FilterValues = Record<Filter['id'], Value>;
+export type FilterValues = Record<Filter['id'], Options>;
 
-export type HandleFilterChange = (value: Value, negated: boolean, id: Filter['id']) => void;
+export type HandleFilterChange = (value: Options, negated: boolean, id: Filter['id']) => void;
