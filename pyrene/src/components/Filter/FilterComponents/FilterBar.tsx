a59a0d8bed6136ac9bae408845b40450e0ea44a3
Sakai-san <o_edo@hotmail.com>
2021-10-12 22:05:12 +0200

    Typing.

--- pyrene/src/components/Filter/FilterComponents/FilterBar.tsx
+++ pyrene/src/components/Filter/FilterComponents/FilterBar.tsx
@@ -7,7 +7,7 @@ import FilterPopoverButton from '../FilterPopOverButton/FilterPopoverButton';
 import FilterTag from './FilterTag';
 import {
   FilterValues,
-  Value,
+  Options,
   Filter,
   MultiselectValue,
   SingleSelectValue,
@@ -86,7 +86,7 @@ export default class FilterBar extends React.Component<FilterBarProps, FilterBar
     }));
   };
 
-  filterDidChange = (value: Value, negated: boolean, key: Filter['id']) => {
+  filterDidChange = (value: Options, negated: boolean, key: Filter['id']) => {
     this.setState((prevState) => ({
       unAppliedFilters: {
         values: { ...prevState.unAppliedFilters.values, [key]: value },
